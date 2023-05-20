4b26ba1ff7813e910d50f2a3a65132e40a9b2293
VICIWUOHA <viciwuoha@gmail.com>
2023-05-20 19:56:49 +0100

    formatted using black

--- src/example_fivetran_dbt.py
+++ src/example_fivetran_dbt.py
@@ -1,4 +1,3 @@
-
 import json
 import os
 import requests
@@ -17,73 +16,75 @@ from fivetran import FivetranApi
 from dbt_cloud import DbtCloudApi
 
 # these are environment variables stored on the virtual environment where airflow is running
-FIVETRAN_API_KEY = os.getenv('FIVETRAN_API_KEY', '')
-FIVETRAN_DATETIME_FORMAT = os.getenv('FIVETRAN_DATETIME_FORMAT', '')
+FIVETRAN_API_KEY = os.getenv("FIVETRAN_API_KEY", "")
+FIVETRAN_DATETIME_FORMAT = os.getenv("FIVETRAN_DATETIME_FORMAT", "")
 
-AIRFLOW_DATETIME_FORMAT = os.getenv('AIRFLOW_DATETIME_FORMAT', '')
+AIRFLOW_DATETIME_FORMAT = os.getenv("AIRFLOW_DATETIME_FORMAT", "")
 
-DBT_ACCOUNT_ID = os.getenv('DBT_ACCOUNT_ID', '')
-DBT_API_KEY = os.getenv('DBT_API_KEY', '')
-DBT_DATETIME_FORMAT = os.getenv('DBT_DATETIME_FORMAT', '')
+DBT_ACCOUNT_ID = os.getenv("DBT_ACCOUNT_ID", "")
+DBT_API_KEY = os.getenv("DBT_API_KEY", "")
+DBT_DATETIME_FORMAT = os.getenv("DBT_DATETIME_FORMAT", "")
 
 # initialize Fivetran API module
-ft = FivetranApi(api_token=FIVETRAN_API_KEY, 
-                 fivetran_datetime_format=FIVETRAN_DATETIME_FORMAT, 
-                 airflow_datetime_format=AIRFLOW_DATETIME_FORMAT)
+ft = FivetranApi(
+    api_token=FIVETRAN_API_KEY,
+    fivetran_datetime_format=FIVETRAN_DATETIME_FORMAT,
+    airflow_datetime_format=AIRFLOW_DATETIME_FORMAT,
+)
 
 # initialize dbt Cloud module
-dbt = DbtCloudApi(account_id=DBT_ACCOUNT_ID, 
-                  api_token=DBT_API_KEY,
-                  airflow_datetime_format=AIRFLOW_DATETIME_FORMAT,
-                  dbt_datetime_format=DBT_DATETIME_FORMAT)
+dbt = DbtCloudApi(
+    account_id=DBT_ACCOUNT_ID,
+    api_token=DBT_API_KEY,
+    airflow_datetime_format=AIRFLOW_DATETIME_FORMAT,
+    dbt_datetime_format=DBT_DATETIME_FORMAT,
+)
 
-args = {
-    'owner': 'airflow',
-    'start_date': datetime.now(),
-    'provide_context': True
-}
+args = {"owner": "airflow", "start_date": datetime.now(), "provide_context": True}
 
 dag = DAG(
-    dag_id='example_fivetran_dbt_operator',
+    dag_id="example_fivetran_dbt_operator",
     default_args=args,
     schedule_interval=None,
     start_date=days_ago(2),
-    tags=['example'],
+    tags=["example"],
 )
 
 run_fivetran_connector_sync = PythonOperator(
-    task_id='fivetran_connector_sync',
+    task_id="fivetran_connector_sync",
     python_callable=ft.force_connector_sync,
     dag=dag,
 )
 
 run_get_connector_sync_status = PythonOperator(
-    task_id='get_connector_sync_status',
+    task_id="get_connector_sync_status",
     python_callable=ft.get_connector_sync_status,
     dag=dag,
 )
 
 run_dbt_job = PythonOperator(
-    task_id='dbt_job',
+    task_id="dbt_job",
     python_callable=dbt.run_job,
     dag=dag,
 )
 
 run_get_dbt_job_status = PythonOperator(
-    task_id='get_dbt_job_status',
+    task_id="get_dbt_job_status",
     python_callable=dbt.get_dbt_job_run_status,
     dag=dag,
 )
 
 run_extract_dbt_job_run_manifest = PythonOperator(
-    task_id='extract_dbt_job_run_manifest',
+    task_id="extract_dbt_job_run_manifest",
     python_callable=dbt.get_job_run_manifest,
     dag=dag,
 )
 
 # create the DAG pipeline by setting dependencies using bitshift operators
-run_fivetran_connector_sync >> \
-run_get_connector_sync_status >> \
-run_dbt_job >> \
-run_get_dbt_job_status >> \
-run_extract_dbt_job_run_manifest
+(
+    run_fivetran_connector_sync
+    >> run_get_connector_sync_status
+    >> run_dbt_job
+    >> run_get_dbt_job_status
+    >> run_extract_dbt_job_run_manifest
+)
